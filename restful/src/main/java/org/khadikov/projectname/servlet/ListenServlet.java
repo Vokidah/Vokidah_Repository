74a1cfb1059399b048ef239ca67791bd322a9698
Vokidah <arsen.khadikov@gmail.com>
2015-06-29 19:15:35 +0300

    Check

--- restful/src/main/java/org/khadikov/projectname/servlet/ListenServlet.java
+++ restful/src/main/java/org/khadikov/projectname/servlet/ListenServlet.java
@@ -30,14 +30,14 @@ public class ListenServlet extends HttpServlet {
     public void init() {
         this.basePath = this.getServletConfig().getInitParameter(BASE_PATH_PARAM);
         String packageName = this.getServletConfig().getInitParameter(PACKAGE_NAME);
-
         this.handlers = new ClassPathScanner(packageName).getRestHandlers();
     }
 
     public void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException, NoClassDefFoundError {
         String reqMethod = req.getMethod();
         String URI = req.getRequestURI();
-
+        Object result=null;
+        Method method;
         if (URI.startsWith(this.basePath)) {
             URI = URI.substring(this.basePath.length(), URI.length());
         }
@@ -49,26 +49,35 @@ public class ListenServlet extends HttpServlet {
                     for (Map.Entry<String, Class> entry : handlers.entrySet()) {
                         String path = entry.getKey();
                         Class handlerClass = entry.getValue();
-
                         if (URI.contains(path)) {
-
                             if (reqMethod.equals(RequestType.GET.value())) {
                                 resp.setContentType("application/json;charset=UTF-8");
-
-                                Method method = getMethod(handlerClass,
-                                        path, RequestType.GET);
-
                                 Object handler = handlerClass.newInstance();
-
-                                Object result = method.invoke(handler, null);
-
+                                if(URI.equals(path) || URI.equals(path+"/")) {
+                                    method = getMethod(handlerClass, path, RequestType.GET, "");
+                                    System.out.println(method);
+                                    result = method.invoke(handler, null);
+                                }
+                                else{
+                                    method = getMethod(handlerClass, path, RequestType.GET, "/:id");
+                                    if(URI.contains(path+"/")) {
+                                        String id = URI.substring(path.length() + 1, URI.length());
+                                        if(id.substring(id.length()-1,id.length()).equals("/"))
+                                            id=id.substring(0,id.length()-1);
+                                        result = method.invoke(handler, id);
+                                    }
+                                }
                                 Gson gson = new Gson();
                                 String responseString = gson.toJson(result);
-
-                                resp.getOutputStream().write(responseString.getBytes());
-                                resp.getOutputStream().close();
+                                if (result != null) {
+                                    resp.getOutputStream().write(responseString.getBytes());
+                                    resp.getOutputStream().close();
+                                } else {
+                                    resp.sendError(404);
+                                }
 
                             } else if (reqMethod.equals(RequestType.POST.value())) {
+
 
                             } else if (reqMethod.equals(RequestType.PUT.value())) {
 
@@ -107,9 +116,7 @@ public class ListenServlet extends HttpServlet {
 
     public Method getMethod(Class handlerClass,
                             String path,
-                            RequestType requestType) {
-        String defaultGetRoute = "/" + handlerClass.getSimpleName().toLowerCase();
-
+                            RequestType requestType,String type) {
         Method[] methods = handlerClass.getDeclaredMethods();
 
         for (int atIndex = 0; atIndex < methods.length; atIndex++) {
@@ -119,20 +126,12 @@ public class ListenServlet extends HttpServlet {
             for (Annotation annotation : methodAnnotations) {
                 if (requestType.equals(RequestType.GET)) {
                     if (annotation instanceof Get) {
-                        Get myAnnotation = (Get) annotation;
-
-                        boolean isEmpty = ((Get) annotation).value().equals("");
-                        if(path.equals(defaultGetRoute) && isEmpty){
+                        boolean isEmpty = ((Get) annotation).value().equals(type);
+                        if (isEmpty)
                             return currentMethod;
-                        }
-
-                        if (path.equals(myAnnotation.value())) {
-                            return currentMethod;
-                        }
                     }
                 } else if (requestType.equals(RequestType.POST)) {
                     if (annotation instanceof Post) {
-
                         Post myAnnotation = (Post) annotation;
                         if (path.equals(""))
                             return currentMethod;
